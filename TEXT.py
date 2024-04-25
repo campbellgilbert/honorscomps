4756dceb20af4b0e571d9fb8791a6cd0ed682d59
Campbell <campbell.gilbert@gmail.com>
2024-04-25 13:19:47 -0700

    tracking efficacy of prompt types

--- TEXT.py
+++ TEXT.py
@@ -14,7 +14,7 @@ dataset = {
     "The idea of a 'community of voices' sounds as though it would work on paper but is rather imperfect when you delve into the different implications it can hold for the community and just what it means to have one. While it could be that we should have something such as a community of voices wherein opinions can be expressed as a whole, one must take a closer look at what that really means. To have a community of voices, that would mean many people whose backgrounds can vary." : ["unclear", "vague", "nonsensical", "cognition", "confusing", "argue"],
     "th United states didnt listen to the small protests thruout the 19th centuri regarding peopels rights until the 20th centry when voices of many drowned out the few. Posters dont matter till their held by millions. And only when we r more involved then not does Congress here and respond. But evenchally with the voice of millons behind the movement the gov listen and change. The millon voices sad black isnt bad and got world to chang ebcause of it.": ["Spelling", "grammar", "logical", "sentence", "structure"]
 }
-
+"""
 responses = []
 correct = 0
 total = 0
@@ -37,4 +37,14 @@ for prompt in prompts:
         total += 1
 
 print("total: ", total)
-print("correct: ", correct)
+print("correct: ", correct)"""
+
+output = "Collaboration can also heavily effect workflow as well as reduce motivation of others if there is no cooperation between everyone. Another example being the engineering field, which requires a heavy amount of collaborative work. They tackle multiple aspects of a singular issue that multiple people have input on. Everyone's work is considered in order to find easier ways to fix the issue. Since this field requires people to listen to each other, if one person tries to overpower the workflow gets disrupted. The small conflict can lead to bigger problems such as not being able to solve the issue, slower work, no other inputs, etc. The forceful behaviour reduces the progression in work and delays issues that would be solved with collaborative work since the workers are resentful."
+keywords = ["collaboration", "progression", "delay", "resent"]
+
+for i in keywords:
+    if i in output.lower():
+        print(i)
+numKeywords = sum(1 for substring in keywords if substring in output.lower())
+
+print(numKeywords)
---
+++ run_eval_promptbench.py
@@ -0,0 +1,105 @@
+import promptbench as pb
+from tqdm import tqdm
+import json
+
+#load dataset
+#FIXME -- collate into 1 file?
+f = open('GRADE_inputs.json')
+dataset = json.load(f)
+q = open('GRADE_prompts.json')
+prompts = json.load(q)
+
+# load the model
+model = pb.LLMModel(model='gpt-3.5-turbo', max_new_tokens=10, temperature=0.0001)
+#model = pb.LLMModel(model='gpt-4', max_new_tokens=300, temperature=0.0001)
+"""
+['google/flan-t5-large', 'llama2-7b', 'llama2-7b-chat', 'llama2-13b', 'llama2-13b-chat', 'llama2-70b', 'llama2-70b-chat', 'phi-1.5', 'phi-2', 'palm', 'gpt-3.5-turbo', 'gpt-4', 'gpt-4-1106-preview', 'gpt-3.5-turbo-1106', 'vicuna-7b', 'vicuna-13b', 'vicuna-13b-v1.3', 'google/flan-ul2', 'gemini-pro']
+"""
+#model = pb.LLMModel(model='llama2-13b-chat', max_new_tokens=10, temperature=0.0001)
+
+def has_any(reply, keywords):
+    return any(substring in reply for substring in keywords)
+
+#1 if response contains ideal keywords, 0 if doesn't contain keywords
+#METRICS TO MEASURE --
+"""
+- efficacy of different prompt types - expert, chain-of-thought, no-yapping
+- performances of different models based on how binary keyword matching
+- obviously efficiencies of different models but we're going to load that into its own set manually and then make some
+
+GRAPHS. 
+FUCK YOU.
+"""
+
+#prompt type efficacies, binary keyword prevalence
+ptes_KEYWORD_BINARY = {
+    "chain-of-thought": 0,
+    "expert": 0,
+    "no-yapping": 0,
+    "baseline": 0,
+}
+
+#prompt type efficacies, number of keywords
+ptes_NUM_KEYWORDS = {
+    "chain-of-thought": 0,
+    "expert": 0,
+    "no-yapping": 0,
+    "baseline": 0,
+}
+
+#prompt type efficacies, keyword-to-text ratio
+ptes_KEYWORD_RATIO = {
+    "chain-of-thought": 0,
+    "expert": 0,
+    "no-yapping": 0,
+    "baseline": 0
+}
+
+hasKeywords = 0
+total = 0
+for prompt in prompts:
+    outputs = []
+
+    predicted_keywords = []
+    actual_keywords = []
+    for data in tqdm(dataset):
+        
+
+        # process input
+        input_text = prompt["prompt"] + " " + data['text_sample'] 
+
+        #get output from model
+        output = model(input_text)
+        outputs.append(output)
+
+        #keyword eval
+        if(has_any(output, data['keywords'])):
+            #a keyword exists, record binary keyword prevalence for prompttype
+            ptes_KEYWORD_BINARY[prompt['type']] += 1
+            
+            #number-keyword eval
+            numKeywords = sum(1 for substring in data['keywords'] if substring in output.lower())
+
+            predicted_keywords.append(len(data['keywords']))
+            actual_keywords.append(numKeywords)
+
+            #keyword prevalence per prompt type
+            #keyword-to-text ratio per prompt type -- length of output divided by number of keywords
+            ptes_KEYWORD_RATIO[prompt['type']] += (numKeywords / len(output))
+
+        total += 1
+        #print("num keywords found: ", numKeywords)
+        
+    # evaluate
+    #score = pb.Eval.compute_cls_accuracy(preds, labels)
+    #print(f"{score:.3f}, {prompt}")
+
+print("keyword predictions vs keyword actuals: ", sum(a == b for a, b in zip(predicted_keywords, actual_keywords)) / len(predicted_keywords))
+print()
+print("TOTAL NUM WITH KEYWORDS: ", sum())
+print("TOTAL NUM: ", total)
+print()
+print("PROMPT TYPE EFFICACIES, binary keyword prevalence: ", ptes_KEYWORD_BINARY)
+print("PROMPT TYPE EFFICACIES, keyword-to-text ratio: ", ptes_KEYWORD_RATIO)
+
+print("good job campbell im so proud of you")
